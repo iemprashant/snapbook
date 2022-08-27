196a2cc8de0305fc24b2cf3c9f82c555d8a40e7a
iemprashant <prashantgupta9876@gmail.com>
2022-08-28 00:48:14 +0530

    Minor change

--- src/Component/UploadPage/UploadPage.js
+++ src/Component/UploadPage/UploadPage.js
@@ -29,6 +29,7 @@ function UploadPage() {
         console.log('Upload is ' + progress + '% done');
       },
       (error) => {
+        console.log(error);
         alert(error);
       },
       () => {
