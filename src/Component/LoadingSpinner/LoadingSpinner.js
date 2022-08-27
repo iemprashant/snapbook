311e1e0757f525cbac9b6dbf1f4cdefbe506ccef
iemprashant <prashantgupta9876@gmail.com>
2022-08-28 02:21:51 +0530

    minor change in loader div

--- src/Component/LoadingSpinner/LoadingSpinner.js
+++ src/Component/LoadingSpinner/LoadingSpinner.js
@@ -2,7 +2,10 @@ import Spinner from 'react-bootstrap/Spinner';
 
 function LoadingSpinner() {
   return (
-    <div className="text-center my-5">
+    <div
+      className=" my-5 d-flex justify-content-center align-items-center"
+      style={{ height: '62.5vh' }}
+    >
       <Spinner animation="border" role="status">
         <span className="visually-hidden">Loading...</span>
       </Spinner>
